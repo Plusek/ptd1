4703513189f0754511823a14dd31d6898f9312d0
Svetlana A <sve3862@yandex.ru>
2024-02-13 22:29:48 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+            int amount=20;
+            int price=1000;
+
+            System.out.println(price/amount);
+
+        }
+    }
